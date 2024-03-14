b59de5a5abf8085bc15015c38179386b7d20a585
vinhdt-2002 <dothanhvinh1372002@gmail.com>
2024-03-14 22:17:20 +0700

    need to fix some error

---
+++ src/viewpage/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'components/ui/button';
+
+function App() {
+  return (
+    <>
+      <Button variant="primary">hehh</Button>
+      <Button>hehh</Button>
+    </>
+  );
+}
+
+export default App;
