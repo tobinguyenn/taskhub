c20044298fe557338570ddc417c802e9f3729bba
vinhdt-2002 <dothanhvinh1372002@gmail.com>
2024-03-16 22:14:29 +0700

    style: Change menu ues react router some page

--- src/viewpage/Contact.js
+++ src/viewpage/Contact.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from 'components/ui/accordion';
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Via Email</AccordionTrigger>
+        <AccordionContent>test@gamil.com</AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Via phone num</AccordionTrigger>
+        <AccordionContent>0123456789</AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Via FB</AccordionTrigger>
+        <AccordionContent>Vinh</AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
 }
 
 export default Contact;
--- src/viewpage/LayoutMenu.js
+++ src/viewpage/LayoutMenu.js
@@ -1,22 +1,39 @@
 import { Link, Outlet } from 'react-router-dom';
-import { Menubar, MenubarMenu, MenubarTrigger } from 'components/ui/menubar';
+import { IoSettings } from 'react-icons/io5';
+import { IoIosSearch } from 'react-icons/io';
 
 function LayoutMenu() {
   return (
     <>
-      <Menubar>
-        <MenubarMenu>
-          <MenubarTrigger>
-            <Link to="/">Home</Link>
-          </MenubarTrigger>
-          <MenubarTrigger>
-            <Link to="/contact">Contact</Link>
-          </MenubarTrigger>
-          <MenubarTrigger>
-            <Link to="/testpage">Testpage</Link>
-          </MenubarTrigger>
-        </MenubarMenu>
-      </Menubar>
+      <nav className="flex justify-between px-20 py-5 items-center bg-white shadow-lg">
+        <h1 className="text-xl text-gray-800 font-bold">TaskHub</h1>
+        <div className="flex items-center">
+          <div className="flex items-center border border-white p-2 mr-3 hover:border-black hover:rounded-xl ">
+            <IoIosSearch />
+            <input
+              className="ml-2 outline-none bg-transparent font-"
+              type="text"
+              name="search"
+              id="search"
+              placeholder="Search..."
+            />
+          </div>
+          <ul className="flex items-center space-x-6">
+            <li className="font-semibold text-gray-700">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="font-semibold text-gray-700">
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li className="font-semibold text-gray-700">
+              <Link to="/testpage">Testpage</Link>
+            </li>
+            <li>
+              <IoSettings />
+            </li>
+          </ul>
+        </div>
+      </nav>
       <Outlet />
     </>
   );
