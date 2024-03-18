02bda2aded45799fb710d2bf4015225f8e7fe235
Nguyen Tuan Anh M (anhnt-2136) <nguyen.tuan.anh-m@sun-asterisk.com>
2024-03-18 18:00:10 +0700

    feat: common components

---
+++ src/pages/App.jsx
@@ -0,0 +1,13 @@
+import Login from './Login';
+import Other from './Other';
+import Register from './Register';
+
+export default function App() {
+  return (
+    <>
+      <Login />
+      <Register />
+      <Other />
+    </>
+  );
+}
